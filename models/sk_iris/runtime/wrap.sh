1825b82ce4ec6eac5b92914928a5f3fdc51ca665
Santiago Molina <s.molina.byd@b-yond.com>
2018-04-23 11:49:34 -0300

    update

--- models/sk_iris/runtime/wrap.sh
+++ models/sk_iris/runtime/wrap.sh
@@ -7,7 +7,7 @@ until docker ps;
 do sleep 3; 
 done; 
 
-docker run -v  ${PWD}:/my_model seldonio/core-python-wrapper:0.6 /my_model SkIris ${VERSION} ${REPO} --image-name=${IMAGE}
+docker run -v  ${PWD}:/my_model seldonio/core-python-wrapper:0.7 /my_model SkIris ${VERSION} ${REPO} --image-name=${IMAGE}
 cd ./build
 ./build_image.sh
 docker images 
