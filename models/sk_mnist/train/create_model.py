9f5a5bb59b6696a235b45cf3b22564faa133640e
smolina74 <smolina_74@yahoo.com>
2018-05-17 18:15:32 -0300

    updates

--- models/sk_mnist/train/create_model.py
+++ models/sk_mnist/train/create_model.py
@@ -3,10 +3,26 @@ from sklearn import datasets, metrics
 from sklearn.utils import shuffle
 from sklearn.datasets import fetch_mldata
 from sklearn.externals import joblib
+from shutil import copyfileobj
+from six.moves import urllib
+from sklearn.datasets.base import get_data_home
+import os
+
+def fetch_mnist(data_home=None):
+    mnist_alternative_url = "https://github.com/amplab/datascience-sp14/raw/master/lab7/mldata/mnist-original.mat"
+    data_home = get_data_home(data_home=data_home)
+    data_home = os.path.join(data_home, 'mldata')
+    if not os.path.exists(data_home):
+        os.makedirs(data_home)
+    mnist_save_path = os.path.join(data_home, "mnist-original.mat")
+    if not os.path.exists(mnist_save_path):
+        mnist_url = urllib.request.urlopen(mnist_alternative_url)
+        with open(mnist_save_path, "wb") as matlab_file:
+            copyfileobj(mnist_url, matlab_file)
 
 if __name__ == '__main__':
-
-    mnist = fetch_mldata('MNIST original', data_home="./mnist_sklearn")
+    fetch_mnist()
+    mnist = fetch_mldata('MNIST original')#, data_home="./mnist_sklearn")
     # To apply a classifier on this data, we need to flatten the image, to
     # turn the data in a (samples, feature) matrix:
     n_samples = len(mnist.data)
