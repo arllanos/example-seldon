73ff8a4a61f747d335fab69f26e4dda563926cba
smolina74 <smolina_74@yahoo.com>
2018-05-24 17:22:32 -0300

    updates

--- models/sk_mnist/output_tx_runtime/SkMnist.py
+++ models/sk_mnist/output_tx_runtime/SkMnist.py
@@ -5,9 +5,9 @@ class SkMnist(object):
         self.class_names = ["class:{}".format(str(i)) for i in range(10)]
 
     def transform_output(self,X,feature_names):
-        print("STARTING NEW TRANSFORMING!!!")
-        print(X)
-        print("END TRANSFORMING")
+        print "STARTING NEW TRANSFORMING!!!"
+        print X
+        print "END TRANSFORMING"
         return X
 
     
