c7ca87675391abc1115387441654b587997b343d
smolina74 <smolina_74@yahoo.com>
2018-05-24 16:25:08 -0300

    updates

--- models/sk_mnist/output_tx_runtime/SkMnist.py
+++ models/sk_mnist/output_tx_runtime/SkMnist.py
@@ -3,10 +3,11 @@ from sklearn.externals import joblib
 class SkMnist(object):
     def __init__(self):
         self.class_names = ["class:{}".format(str(i)) for i in range(10)]
-        #self.clf = joblib.load('/data/sk.pkl') 
 
     def transform_output(self,X,feature_names):
+        print("STARTING NEW TRANSFORMING!!!")
         print(X)
+        print("END TRANSFORMING")
         return X
 
     
