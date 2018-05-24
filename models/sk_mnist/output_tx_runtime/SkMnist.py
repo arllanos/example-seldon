c25574a2a0e7f47db78a9ec6adc8e43e495a37cc
smolina74 <smolina_74@yahoo.com>
2018-05-24 18:04:09 -0300

    updates

--- models/sk_mnist/output_tx_runtime/SkMnist.py
+++ models/sk_mnist/output_tx_runtime/SkMnist.py
@@ -7,6 +7,10 @@ class SkMnist(object):
     def transform_output(self,X,feature_names):
         print "STARTING NEW TRANSFORMING!!!"
         print X
+        with open('/data/output.txt', 'w') as f:
+            f.write(str(type(X)))
+            f.write(str(X))
+
         print "END TRANSFORMING"
         return X
 
