37866fe0b7ce40cf23fbcc7f64786302975cb37e
smolina74 <smolina_74@yahoo.com>
2018-05-28 07:56:56 -0300

    update

--- models/sk_mnist/output_tx_runtime/SkMnist.py
+++ models/sk_mnist/output_tx_runtime/SkMnist.py
@@ -9,20 +9,8 @@ class SkMnist(object):
         print "STARTING NEW TRANSFORMING input!!!"
         
         print "END TRANSFORMING"
-        return X
-
-    def transform_output(self,X,feature_names):
-        print "STARTING NEW TRANSFORMING!!!"
-        print X
-        with open('/data/output.txt', 'w') as f:
-            f.write(str(type(X)))
-            f.write(str(X))
-
-        print "END TRANSFORMING"
-
         predictions = [[0.99, 0.1, 0.03333333333333333, 0.03333333333333333, 0.06666666666666667, 0.1, 0.03333333333333333, 0.5333333333333333, 0.0, 0.03333333333333333]]
         return predictions
-
         
 
     
