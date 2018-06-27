b712aad766b3e17ffecac2b17a9ea6a5b7512f22
smolina74 <smolina_74@yahoo.com>
2018-06-27 10:37:29 -0300

    update

--- models_examples/sk_mnist/runtime/SkMnist.py
+++ models_examples/sk_mnist/runtime/SkMnist.py
@@ -8,7 +8,7 @@ class SkMnist(object):
         #self.clf = joblib.load('/data/sk.pkl') 
 
     def predict(self,X,feature_names):
-        #predictions = self.clf.predict_proba(X)
+        print X
         predictions = [[0.06666666666666667, 0.1, 0.03333333333333333, 0.03333333333333333, 0.06666666666666667, 0.1, 0.03333333333333333, 0.5333333333333333, 0.0, 0.03333333333333333]]
         return predictions
 
