471b59a5077e449cebbddc2494675efab4df29bc
Santiago Molina <s.molina.byd@b-yond.com>
2018-06-30 23:25:58 -0300

    update

--- models_examples/sk_mnist/runtime/SkMnist.py
+++ models_examples/sk_mnist/runtime/SkMnist.py
@@ -6,7 +6,7 @@ class SkMnist(object):
     def __init__(self, **kwargs):
         json_conf = os.environ['config_dict'].replace('||','"')
         config = json.loads(json_conf)
-        self.clf = joblib.load('/data/{model_id}_{model_version}_{owner_id}_{data_version}/SVM.pkl'.format(**config)) 
+        self.clf = joblib.load('/data/{model_id}-{model_version}-{owner_id}-{data_version}/SVM.pkl'.format(**config)) 
 
     def predict(self,X,feature_names):
         predictions = self.clf.predict(X)
