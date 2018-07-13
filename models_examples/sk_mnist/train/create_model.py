02c3a85fef493df9bc1da26e87c5e99681c1af1a
arllanos <ariel.llanos@gmail.com>
2018-07-13 16:55:41 -0300

    Alternative training workflow and sk model

--- models_examples/sk_mnist/train/create_model.py
+++ models_examples/sk_mnist/train/create_model.py
@@ -3,14 +3,28 @@ from sklearn import svm, metrics
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from sklearn.externals import joblib
+import os
 
 if __name__ == '__main__':
-    data_dir = 'tmp/tensorflow/mnist/input_data'
+    PERSISTENT_VOLUME = '/data'
+
+    data_dir = '%s/tmp/tensorflow/mnist/input_data' % PERSISTENT_VOLUME
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
     mnist = input_data.read_data_sets(data_dir, one_hot=True)
     train_data=mnist.train.images
     train_labels=np.array(np.where(mnist.train.labels==1))[1]
+
     classifier = svm.SVC(gamma=0.001)
     classifier.fit(train_data, train_labels)
-    joblib.dump(classifier, 'data/models/mnist_svm_0_0/SVM.pkl')
 
-
+    model_id = 'mnist'
+    model_version = 'svm'
+    owner_id = '1'
+    data_version = '0'
+    dump_dir = '%s/%s-%s-%s-%s' % (PERSISTENT_VOLUME, model_id, model_version, owner_id, data_version)
+    if not os.path.exists(dump_dir):
+        os.makedirs(dump_dir)
+    fullfilename = '%s/%s' % (dump_dir, '%s.pkl' % model_version)
+    joblib.dump(classifier, fullfilename)
