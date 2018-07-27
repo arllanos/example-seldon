80f80076b58eb855ec66971773da124fbb24cc85
Santiago Molina <s.molina.byd@b-yond.com>
2018-07-27 15:05:56 -0300

    updates

--- models_examples/mnistconsumer/runtime/MnistConsumer.py
+++ models_examples/mnistconsumer/runtime/MnistConsumer.py
@@ -1,4 +1,14 @@
+import logging 
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 class MnistConsumer(object):
     def predict(self,X,feature_names):
-        print("Receiving " + str(X))
+        logger.info("Receiving " + str(X))
         return X
