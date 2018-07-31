bd2906cadf4e9fceb75439aa07b4f9d33470d0aa
Santiago Molina <s.molina.byd@b-yond.com>
2018-07-31 10:44:54 -0300

    adding feature collection for mnistconsumer

---
+++ models_examples/mnistconsumer/collection/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, Response
+import json
+import logging
+import sys
+
+logging.basicConfig(level=logging.DEBUG)
+
+app = Flask(__name__)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+@app.route('/api/features', methods=['GET'])
+def get():
+    ids = json.loads(request.data)
+    logger.info("Received IDs: " + str(ids))
+    return Response(json.dumps([1,2,3]), mimetype=u'application/json')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ models_examples/mnistconsumer/collection/build_and_push.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+VERSION=$1
+REPO=$2
+
+IMAGE=mnistconsumer_feature_collection
+
+until docker ps; 
+do sleep 3; 
+done; 
+
+docker build --force-rm=true -t ${REPO}/${IMAGE}:${VERSION} . 
+docker images 
+echo "Pushing image to ${REPO}/${IMAGE}:${VERSION}"
+echo $DOCKER_PASSWORD | docker login --username=$DOCKER_USERNAME --password-stdin 
+docker push ${REPO}/${IMAGE}:${VERSION}
