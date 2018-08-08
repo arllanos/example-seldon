a948316f8a5902d6c4157949ed0441f07476e3db
Santiago Molina <s.molina.byd@b-yond.com>
2018-08-08 10:15:01 -0300

    changes in SkMnist for supporting the array of dicts as samples and adding Dockerfile to mnist consumer

--- models_examples/sk_mnist/runtime/SkMnist.py
+++ models_examples/sk_mnist/runtime/SkMnist.py
@@ -9,6 +9,14 @@ class SkMnist(object):
         self.clf = joblib.load('/data/{model_id}-{model_version}-{owner_id}-{data_version}/SVM.pkl'.format(**config)) 
 
     def predict(self,X,feature_names):
-        predictions = self.clf.predict(X).reshape(1, -1)
-        return predictions
+        # X is a numpy array containing dictionaries of the form
+        # {'id':<ID>, 'values': <features>}
+        # So each dictionary contain the sample to be use by one prediction plus
+        # some identification identifying that sample.
+        ids = [elem['id'] for elem in X]
+        feature_matrix = np.array([elem['values'] for elem in X])
+        predictions = self.clf.predict(feature_matrix).reshape(1, -1)
+        predictions_with_ids = [{'id': elem[0], 'values': elem[1]} for elem in zip(ids, feature_matrix)]
+        return predictions_with_ids
 
+g
