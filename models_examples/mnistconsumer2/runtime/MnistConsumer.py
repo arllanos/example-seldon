469a8965ff2879a6aa9ac38f11ce25461e1667c3
smolina74 <s.molina.byd@b-yond.com>
2018-08-11 18:17:05 -0300

    extra model for testing/comparing deployment performance

---
+++ models_examples/mnistconsumer2/runtime/MnistConsumer.py
@@ -0,0 +1,15 @@
+import logging 
+import sys
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+class MnistConsumer(object):
+    def predict(self,X,feature_names):
+        logger.info("Receiving " + str(X))
+        return X
